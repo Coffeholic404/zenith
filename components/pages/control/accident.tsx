5ca0bb7789092ed3aa4402acb8294a4b8174484b
Taqe <taqealibade@gmail.com>
2025-09-19 17:47:05 +0300

    feat(accident): add hover animation to black hole images
    Add group-hover animation to spin black hole images when hovering over their containers to improve visual feedback

--- components/pages/control/accident.tsx
+++ components/pages/control/accident.tsx
@@ -5,22 +5,22 @@ import { Eye } from "lucide-react";
 export default function Accident () {
     return (
         <div className=" flex flex-col gap-4">
-            <div className=" flex items-center justify-between border rounded-lg p-4">
+            <div className=" flex items-center justify-between border rounded-lg p-4 group">
                     <div className=" space-y-1 ">
                         <p className=" font-vazirmatn font-normal text-accidentTxtClr text-lg">تأخير في فتح المظلة</p>
                         <p className=" font-vazirmatn font-normal text-[#868585] text-sm ">2025 - 07 - 20 <span> .09:00</span></p>
                     </div>
                     <div className="bg-[#F5A7C0]  p-2 rounded-xl grid place-content-center">
-                    <Image src={blackHole} alt="black hole"  />
+                    <Image src={blackHole} alt="black hole"  className=" group-hover:animate-spin"/>
                     </div>
             </div>
-            <div className=" flex items-center justify-between border rounded-lg p-4">
+            <div className=" flex items-center justify-between border rounded-lg p-4 group">
                     <div className=" space-y-1 ">
                         <p className=" font-vazirmatn font-normal text-accidentTxtClr text-lg">تأخير في فتح المظلة</p>
                         <p className=" font-vazirmatn font-normal text-[#868585] text-sm ">2025 - 07 - 20 <span> .09:00</span></p>
                     </div>
                     <div className="bg-[#F5A7C0]  p-2 rounded-xl grid place-content-center">
-                    <Image src={blackHole} alt="black hole"  />
+                    <Image src={blackHole} alt="black hole"  className=" group-hover:animate-spin"/>
                     </div>
             </div>
             <Button className=" bg-updatesBtnClr text-btnTxtClr hover:bg-updatesBtnClr">
